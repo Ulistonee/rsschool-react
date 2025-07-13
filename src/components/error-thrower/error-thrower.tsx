ef9b83747b9f6f29a3747f3559330e306a82f46a
Ulistonee <ulistonee@gmail.com>
2025-07-14 00:21:12 +0500

    chore: test husky

--- src/components/error-thrower/error-thrower.tsx
+++ src/components/error-thrower/error-thrower.tsx
@@ -6,8 +6,6 @@ type State = { throwError: boolean };
 class ErrorThrower extends Component<Props, State> {
   state: State = { throwError: false };
 
-  a = 0;
-
   render() {
     if (this.state.throwError) {
       throw new Error('Manual error');
