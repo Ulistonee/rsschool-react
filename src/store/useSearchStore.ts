461f89bd059cb2f96b105bbda3d3369a415d4d08
Ulistonee <ulistonee@gmail.com>
2025-08-03 23:53:25 +0500

    feat: make slice for zustand

--- src/store/useSearchStore.ts
+++ src/store/useSearchStore.ts
@@ -1,26 +1,9 @@
 import { create } from 'zustand';
-import type { Person } from '../types/person';
-import { getId } from '../utils/getId.ts';
+import type { SearchSlice } from './slices/searchSlice';
+import { createSearchSlice } from './slices/searchSlice';
 
-type SearchStore = {
-  selectedPeople: Record<string, Person>;
-  selectPerson: (person: Person) => void;
-  unselectPerson: (id: string) => void;
-  clearSelection: () => void;
-};
-
-const useSearchStore = create<SearchStore>((set) => ({
-  selectedPeople: {},
-  selectPerson: (person) =>
-    set((state) => ({
-      selectedPeople: { ...state.selectedPeople, [getId(person.url)]: person },
-    })),
-  unselectPerson: (id) =>
-    set((state) => {
-      const { [id]: _, ...rest } = state.selectedPeople;
-      return { selectedPeople: rest };
-    }),
-  clearSelection: () => set({ selectedPeople: {} }),
+export const useSearchStore = create<SearchSlice>()((set) => ({
+  ...createSearchSlice(set),
 }));
 
 export default useSearchStore;
