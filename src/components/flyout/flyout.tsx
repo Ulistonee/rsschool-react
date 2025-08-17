7a7ad82e6db77f4888eb275dbddb66d9736b294c
Ulistonee <ulistonee@gmail.com>
2025-08-18 02:20:07 +0500

    feat: make csv generation server

--- src/components/flyout/flyout.tsx
+++ src/components/flyout/flyout.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import styles from './flyout.module.css';
-import { handleDownload } from '../../utils/handleDownload.ts';
 import type { Person } from '../../types/person.ts';
 import { useTranslations } from 'next-intl';
+import { generateCsv } from '../../app/actions/export.ts';
+import { saveAs } from 'file-saver';
 
 type Props = {
   selectedPeople: Record<string, Person>;
@@ -10,6 +13,13 @@ type Props = {
 
 export const Flyout = ({ selectedPeople, clearSelection }: Props) => {
   const t = useTranslations('Flyout');
+  const length = Object.keys(selectedPeople)?.length;
+
+  const handleClick = async (selectedPeople: Record<string, Person>) => {
+    const blob = await generateCsv(selectedPeople);
+    const fileName = `${length}_person.csv`;
+    saveAs(blob, fileName);
+  };
 
   return (
     <div className={styles.flyout}>
@@ -17,7 +27,7 @@ export const Flyout = ({ selectedPeople, clearSelection }: Props) => {
         {Object.keys(selectedPeople).length} {t('personSelected')}
       </p>
       <button onClick={clearSelection}>{t('unselect')}</button>
-      <button onClick={() => handleDownload(selectedPeople)}>
+      <button onClick={() => handleClick(selectedPeople)}>
         {t('download')}
       </button>
     </div>
