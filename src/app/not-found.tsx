db0e06364093fccffa3ba341330c01cd50dfda07
Ulistonee <ulistonee@gmail.com>
2025-08-17 18:42:36 +0500

    fix: fix 404 global page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,26 @@
+'use client';
+
+import classNames from 'classnames';
+import styles from './[locale]/not-found.module.css';
+import Link from 'next/link';
+import { useTheme } from '../context/theme-context.tsx';
+
 export default function NotFound() {
-  return <div>not found</div>;
+  const { theme } = useTheme();
+
+  return (
+    <div className={classNames(styles[theme], styles.container)}>
+      <h1>404 — Page not found</h1>
+      <p>
+        Check the address or return to{' '}
+        <Link
+          href="/en"
+          className={classNames(styles.navLink, styles.resetLink)}
+        >
+          home
+        </Link>
+        .
+      </p>
+    </div>
+  );
 }
